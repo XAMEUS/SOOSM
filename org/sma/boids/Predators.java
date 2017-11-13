20dbc82e30d57490d32030efcd7f77addb5fb506
Maxime <maxime.gourgoulhon@gmail.com>
2017-11-13 11:05:31 +0100

    rename Predators

--- org/sma/boids/Predators.java
+++ org/sma/boids/Predators.java
@@ -3,10 +3,10 @@ package org.sma.boids;
 import java.awt.Color;
 import java.util.Iterator;
 
-public class Predator extends Boids {
+public class Predators extends Boids {
 	private Boids victims;
 
-	public Predator(int n, int minx, int miny, int maxx, int maxy, int max_speed, Color color, Boids victims) {
+	public Predators(int n, int minx, int miny, int maxx, int maxy, int max_speed, Color color, Boids victims) {
 		super(n, minx, miny, maxx, maxy, max_speed, color);
 		this.setCohesion(1, 700, 0.01, 10);
 		this.setAlignment(0, 500);
